918bc5fadb3c5ddc0a1e082167a0e9e4ce582a52
Andrew Zimmerman <azimmerman@pga.com>
2023-02-12 11:22:08 -0500

    Initial Commit

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+//  Moduels and routes
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+// middleware
+
+
+// controllers and routes
+
+
+// Mongo Connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
+
+// Configue Port and listen
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, console.log(`Listening on Port ${PORT}`))
+
+module.exports = app;
