011fca7c8e7e3eeb5197a1f86deae5b8c599752e
Andrew Zimmerman <azimmerman@pga.com>
2023-02-14 20:27:06 -0500

    imported controllers to the server

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,13 @@ const mongoose = require('mongoose')
 const app = express()
 
 // middleware
-
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOveride('_method'))
 
 // controllers and routes
-
+const placesRoutes = require('./controllers/restaurants')
+app.use('/places', placesRoutes)
 
 // Mongo Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
