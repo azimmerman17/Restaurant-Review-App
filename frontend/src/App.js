b7e7cc968a5e36afab58e860e475798d92504c33
Andrew Zimmerman <azimmerman@pga.com>
2023-02-16 19:42:00 -0500

    Added navbar and routing to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './Components/NavBar';
+
 
 function App() {
   return (
-    <div className="App"> 
+    <div className="App">
+      <Router>
+        <header>
+          <NavBar />
+        </header>
+        <main>
       Restaurant App    
-      {/* Navbar */}
-      {/* Router */}
-      {/* Routes with views */}
+          <Routes>
+            {/* <Route exact path='/' element={<Home />} />  */}
+            {/* <Route path='/places' element={<Places />} /> */}
+            {/* <Route path 'places/new' element={<PlacesForm />} /> */}
+            {/* <Route path='/places/:id' element={<Show />} /> */}
+            {/* <Route path='/places/:id/edit' element={<PlacesForm/>} /> */}
+            {/* <Route path='/*' element={<Error404 />} /> */}
+          </Routes>
+
+        </main>
+      </Router>
 
     </div>
   );
